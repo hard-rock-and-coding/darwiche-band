fe5e6d1a76528dc6f55daa10965b59360f7c0614
hard-rock-and-coding <jays.darwiche@gmail.com>
2022-03-06 01:33:47 +0000

    Convert images to webp - remove un-needed cdn scripts for bootstrap

--- darwiche-site/src/components/Header.js
+++ darwiche-site/src/components/Header.js
@@ -1,10 +1,10 @@
 import React from "react";
-import logoName from "../assets/darwiche_name_transparent.png"
+import logoName from "../assets/darwiche_name_transparent.webp"
 
 const Header = () => {
     return (
       <header className="d-flex flex-row p-3">
-        <img src={logoName} alt="darwiche" height="45"/>
+        <img src={logoName} alt="darwiche" height="45" width="273"/>
       </header>
     );
   };
